2bd2dd6045bda4243a32a3b9f9376d1b8188591e
lxng <lxng@lxng>
2025-04-12 22:47:38 +0700

    add

---
+++ src/l3_MangVaPhuongThucTrongJava/DemSoLanXuatHienCuaKyTuTrongChuoi.java
@@ -0,0 +1,25 @@
+package l3_MangVaPhuongThucTrongJava;
+
+import java.util.Scanner;
+
+public class DemSoLanXuatHienCuaKyTuTrongChuoi {
+    public static void main(String[] args) {
+        String str = "how are you";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap vao ky tu can dem: ");
+        char c = scanner.next().charAt(0);
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+
+        System.out.println("Ky tu '" + c + "' xuat hien " + count + " lan trong chuoi.");
+        scanner.close();
+    }
+}
+
