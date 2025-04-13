9698e662d9ea7d03490911ec467936c847778a4f
lxng <lxng@lxng>
2025-04-13 22:22:30 +0700

    add

---
+++ src/l5_AccessModifier_StaticMethod_StaticProperty/Car.java
@@ -0,0 +1,13 @@
+package l5_AccessModifier_StaticMethod_StaticProperty;
+
+public class Car {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+}
