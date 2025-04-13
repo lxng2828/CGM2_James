f0e83fab029ab86edfa28770d37edab244bc2cec
lxng <lxng@lxng>
2025-04-13 22:20:03 +0700

    add

---
+++ src/l5_AccessModifier_StaticMethod_StaticProperty/StaticMethod.java
@@ -0,0 +1,19 @@
+package l5_AccessModifier_StaticMethod_StaticProperty;
+
+public class StaticMethod {
+    public static void main(String args[]) {
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+
+        s1.display();
+        s2.display();
+        s3.display();
+
+        Student.change();
+
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
---
+++ src/l5_AccessModifier_StaticMethod_StaticProperty/Student.java
@@ -0,0 +1,20 @@
+package l5_AccessModifier_StaticMethod_StaticProperty;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    Student(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+    static void change() {
+        college = "CODEGYM";
+    }
+
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
